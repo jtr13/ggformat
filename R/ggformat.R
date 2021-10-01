69fe639d033bc001ccd3d53a1540f0f5d161727c
Joyce Robbins <jtr13@columbia.edu>
2021-10-01 13:27:08 -0400

    first working version

---
+++ R/ggformat.R
@@ -0,0 +1,34 @@
+#' Reformat ggplot2 code
+#'
+#'
+#' @export
+
+FormatCode <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  text <- context$selection[[1]]$text
+  if(nchar(text) == 0) stop("The selection is empty -- make sure the cursor is in the highlighted selection before running the addin.")
+  text <- paste(text, collapse = "") # combine into one string
+  text <- trimws(gsub("\\n|\\+", "", text)) # remove line breaks, +,  and trailing white space
+  text <- gsub("\\(\\s+", "\\(", text) # remove spaces after (
+  text <- gsub("\\s+\\)", "\\)", text) # remove spaces before )
+  text <- gsub("\\,\\s+", "\\, ", text) # remove extra spaces after ,
+
+  words <- c("firstline", "geom", "stat", "coord", "facet", "scale", "xlim", "ylim", "ggtitle", "labs", "xlab", "ylab", "theme_", "theme")
+  words_regex <- paste(words, sep = "", collapse = "|")
+  # split at ggplot2 functions, keeping delimiters
+  text <- strsplit(text, paste0("(?<=.)(?=", words_regex, ")"), perl = TRUE)
+  text <- trimws(unlist(text))
+
+  df <- data.frame(text)
+  nr <- nrow(df)
+  # create a column of ggplot2 functions
+  df$func <- sub(paste(".*(", words_regex, ").*", sep=""), "\\1", text)
+  df$func[1] <- "firstline" # override any matches in first line
+  df$func <- factor(df$func, levels = words) # set factor levels
+  df <- df[order(df$func, df$text),] # order by factor level
+
+  df$text[1:(nr-1)] <- paste(df$text[1:(nr-1)], "+") # add + to end of lines except last line
+  df$text[2:nr] <- paste("    ", df$text[2:nr]) # index 2nd line on
+  rstudioapi::selectionSet(value = df$text, id = context$id) # replace text
+
+}
