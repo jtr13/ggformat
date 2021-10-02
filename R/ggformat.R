d4a0b2baec6f9ea212cf7279dddcd9f63b17ab8d
Joyce Robbins <jtr13@columbia.edu>
2021-10-02 10:27:51 -0400

    change how order of words is saved

--- R/ggformat.R
+++ R/ggformat.R
@@ -4,8 +4,11 @@
 #' @export
 
 FormatCode <- function() {
+
   context <- rstudioapi::getActiveDocumentContext()
+
   text <- context$selection[[1]]$text
+
   if(nchar(text) == 0) stop("The selection is empty -- make sure the cursor is in the highlighted selection before running the addin.")
 
   # GENERAL PREP AND CLEAN UP BEFORE SPLITTING LINES
@@ -47,7 +50,7 @@ FormatCode <- function() {
   # set sort order
   orderwords <- c("firstline", "%>%", "ggplot", splitwords)
 
-   # save(orderwords, file = "words.rda")  # uncomment, run, and knit Readme.Rmd if orderwords are changed
+   # writeLines(orderwords, "orderwords.txt")  # uncomment, run, and knit Readme.Rmd if orderwords are changed
 
   order_regex <- paste(orderwords, sep = "", collapse = "|")
 
